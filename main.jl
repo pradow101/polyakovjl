dc362b7fff3a3beaf4b30f3e7ac4cd8643f04d08
João A. Prado <joao.prado@acad.ufsm.br>
2025-04-09 22:50:21 -0300

    Solver updated on main

--- main.jl
+++ main.jl
@@ -5,13 +5,22 @@ begin
     include("functions.jl")
 end
 
-function gapsolver(mu,T)
-    sistema = nlsolve(x->(dM(x[1],x[2],mu,T,x[3]),dphi(x[1],x[2],mu,T,x[3]),dphib(x[1],x[2],mu,T,x[3])), [0.01,0.01,0.3]).zero
+function gapsolver(mu,T,chuteinit)
+    sistema = nlsolve(x->(dM(x[1],x[2],mu,T,x[3]),dphi(x[1],x[2],mu,T,x[3]),dphib(x[1],x[2],mu,T,x[3])),chuteinit)
+    return sistema.zero
 end
-
 
 begin
-    M_vals = range(0,0.5,length=50)
-    yi = [dM(1e-8, 1e-8, 0.2, 0.01, Mi) for Mi in M_vals]
-    plot(M_vals, yi, label="dM", xlabel="M", ylabel="dM", title="dM vs M", grid=true, gridalpha=0.5)
+    T_vals = range(1e-8, 0.35, length = 100)
+    phi_vals = zeros(length(T_vals))
+    mu = 0
+    chuteinit = [0.01,0.01,0.3]
+    Threads.@threads for i in 1:length(T_vals)
+        T = T_vals[i]
+        solution = gapsolver(mu,T)
+        phi_vals[i] = solution[1]
+        chuteinit = solution
+    end
+    plot(T_vals, phi_vals)
 end
+
