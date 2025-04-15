ece4913b73ff548c6a231752c6006361b627860e
João A. Prado <joao.prado@acad.ufsm.br>
2025-04-15 17:37:47 -0300

    Changing the initial guess mid algo run

--- main.jl
+++ main.jl
@@ -1,31 +1,40 @@
-
 begin
     using QuadGK, Plots, NLsolve, CSV, DataFrames, ForwardDiff
     include("parameters.jl")
     include("functions.jl")
 end
 
-function gapsolver(mu,T,chuteinit)
-    sistema = nlsolve(x->(dM(x[1],x[2],mu,T,x[3]),dphi(x[1],x[2],mu,T,x[3]),dphib(x[1],x[2],mu,T,x[3])),chuteinit)
-    return sistema.zero
+function gapsolver(mu,T,chutealto,chutebaixo)
+    sistema1 = nlsolve(x->(dM(x[1],x[2],mu,T,x[3]),dphi(x[1],x[2],mu,T,x[3]),dphib(x[1],x[2],mu,T,x[3])),chutealto)
+    sistema2 = nlsolve(x->(dM(x[1],x[2],mu,T,x[3]),dphi(x[1],x[2],mu,T,x[3]),dphib(x[1],x[2],mu,T,x[3])),chutebaixo)
+    if sistema1.zero[1] > sistema2.zero[1] && sistema1.zero[2] > sistema2.zero[2] && sistema1.zero[3] < sistema2.zero[3]
+        return sistema2.zero
+    else
+        return sistema1.zero
+    end  
 end
 
 begin
-    T_vals = range(0.1, 0.4, length = 100) #range for T
+    T_vals = range(0.01, 0.35, length = 100) #range for T
     phi_vals = zeros(length(T_vals)) # Arrays which will store the phi, phib and M solutions
     phib_vals = zeros(length(T_vals))
     M_vals = zeros(length(T_vals))
-    mu = 0
-    chuteinit = [0.01,0.01,0.3]
-     for i in 1:length(T_vals) #Initial guess
+    mu = 0.34
+    chutealto = [0.01,0.01,0.3]
+    chutebaixo = [1,1,0.01]
+    for i in 1:length(T_vals)-1 #Initial guess
         T = T_vals[i]  #Tells the program to use the ith value of the T_vals array
-        solution = gapsolver(mu, T, chuteinit) #Call gapsolver function, store it in the variable solution
+        solution = gapsolver(mu, T, chutealto, chutebaixo) #Call gapsolver function, store it in the variable solution
         phi_vals[i] = solution[1] #solution is a vector of 3 floats, and we are storing the first one in phi_vals[i],
         phib_vals[i] = solution[2] #the second one in phib_vals[i], and the third one in M_vals[i]
         M_vals[i] = solution[3]
-        chuteinit = solution #update the initial guess with the previous solution
+        if M_vals[i+1] - M_vals[i] > 0.001
+            chutebaixo = solution #update the initial guess with the previous solution
+        else
+            chutealto = solution
+        end
     end
-    plot(T_vals, [M_vals, phi_vals,phib_vals], grid=true, gridalpha=0.5, xlabel = "T", ylabel = "phi, M", title = "M and phi solutions")
+    plot(T_vals, [M_vals,phi_vals,phib_vals], grid=true, gridalpha=0.5, xlabel = "T", ylabel = "phi, M", title = "M and phi solutions")
 end
 
         
@@ -40,6 +49,8 @@ begin       #calculating and plotting the pressure
     end
     plot(T_vals, pf_vals, grid = true, gridalpha=0.5, xlabel = "T", ylabel = "Pressure", title = "Pressure vs T")
 end
+
+#Begin to implement derivatives and phase diagram
 
 begin
     
